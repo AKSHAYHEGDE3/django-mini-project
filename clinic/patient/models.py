a94cda3d737e7afe6a87ea8931b8bf3268dc4bf7
Akshay <akshayhegde158@gmail.com>
2021-05-14 11:08:08 +0530

    ...

--- clinic/patient/models.py
+++ clinic/patient/models.py
@@ -7,3 +7,7 @@ class Patient(models.Model):
     patient_mobile_no=models.CharField(max_length=10)
     patient_email=models.EmailField()
     patient_appointment=models.DateField(("Date"), default=datetime.date.today)
+
+    def __str__(self):
+        return self.patient_name
+    
--- clinic/patient/views.py
+++ clinic/patient/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Patient
-
 # Create your views here.
 # def patient(request):
 #     return render(request,'patient.html')
@@ -21,8 +20,9 @@ def patient(request):
 def success(request):
     return render(request,'success.html')
 
- 
-                 
+x=Patient.objects.all()
+print(x)
+
 
        
                 
